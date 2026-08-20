2a5b65ebd028b25d934c608f32b10c85b54c21ad
nym-blip <camryn.potter25@gmail.com>
2026-08-20 11:06:56 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const CACHE="cam-rpg-v201";self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+const CACHE="cam-rpg-v21";self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
